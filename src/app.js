ae2cce4320892a5c534715bc6638c637aa374dc4
Anderson <anderson771218@gmail.com>
2019-08-11 20:16:12 +0800

    Init project

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+//app.com
+//app.com/help
+//app.com/about
+
+//must be absolute path
+// console.log(__dirname);
+// console.log(path.join(__dirname, '../public'));
+
+//from root /
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('', (req, res) => {
+    res.send('<h2>hellooooo!</h2>');
+});
+
+
+app.listen(3000, () => {
+    console.log('start listen');
+});
