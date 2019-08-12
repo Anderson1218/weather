f541b2d420280127f52c9dc1657937490c1060cd
Anderson <anderson771218@gmail.com>
2019-08-12 14:15:32 +0800

    Structuring pages by partials

--- src/app.js
+++ src/app.js
@@ -1,35 +1,40 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 
-//app.com
-//app.com/help
-//app.com/about
 
-//must be absolute path
-// console.log(__dirname);
-// console.log(path.join(__dirname, '../public'));
+//define path for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
-//from root /
+//setup handlebars engine and views location
 app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, '../public')));
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//setup static directory to serve
+app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
     res.render('index',{
-        title: 'hello',
+        title: 'hello world',
         name: 'Anderson'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about', {
-
+        title: 'about',
+        name: 'Anderson'
     });
 });
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        
+        title: 'help',
+        name: 'Anderson'
     });
 });
 
