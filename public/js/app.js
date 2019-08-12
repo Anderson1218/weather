52892bc6e5a124f21aa34c8b65dbc00f15c14c88
Anderson <anderson771218@gmail.com>
2019-08-13 01:32:09 +0800

    render fetched data in viewport

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,23 @@
-//console.log('hello');
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const msg1 = document.querySelector('#message-1');
+const msg2 = document.querySelector('#message-2');
+
+
+weatherForm.addEventListener('submit', (e) => {
+    msg1.textContent = 'loading...';
+    e.preventDefault();
+    const location = search.value;
+    fetch(`http://localhost:3000/weather?address=${location}`)
+    .then(response => response.json())
+    .then(data => {
+        if(data.error) {
+            msg1.textContent = data.error;
+            msg2.textContent = '';
+        } else {
+            msg2.textContent = data.forecast + data.location;
+            msg1.textContent = '';
+        }
+    });
+
+});
